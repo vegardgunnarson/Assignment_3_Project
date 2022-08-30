e6439404e6bdae9b22a5807cdd922fda571717bf
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-08-30 13:55:50 +0200

    add project

---
+++ src/test/java/com/example/assignment_3/Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
